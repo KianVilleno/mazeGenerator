9fb469cddc003b3468ee7e0fe1f03dab0773e965
Kian <villenokian>
2021-04-19 19:57:02 +1000

    added adjustable size

--- sketch.js
+++ sketch.js
@@ -1,15 +1,22 @@
-const col = 20;
-const row = 20;
-const size = 20;
+let col = 20;
+let row = 20;
+let size = 20;
 let maze;
 let current;
 let stack = [];
 
 function setup() {
+  // Adjustable Size
+
+  if (col * size >= window.innerWidth) {
+    col = Math.floor(window.innerWidth / size) - 1;
+    row = col;
+  }
   //Canvas
   createCanvas(col * size, row * size);
 
   //Making the Grid
+
   maze = new GRID(col, row, size);
   maze.createGRID();
   current = maze.cells[0];
